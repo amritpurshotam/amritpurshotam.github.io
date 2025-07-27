d1cf606faf532c0b0699ce5d6dbd4f8bb6acc86f
amritpurshotam <amritpurshotam@users.noreply.github.com>
2025-07-27 17:44:45 +0000

    Deploying to gh-pages from @ amritpurshotam/amritpurshotam.github.io@769462a5240aefdadf8347c42d4dca7da2f4a294 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -27,12 +27,7 @@ ninja.data = [{
             window.location.href = "/blog/2025/coming-soon/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{
+      },{
         id: 'social-bluesky',
         title: 'Bluesky',
         section: 'Socials',
